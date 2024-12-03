77623ed369765707aa858e9114a1d5dcad2e440e
BugHunter-X <askzenai@gmail.com>
2024-12-03 09:52:36 +0100

    Add index.js file

---
+++ index.js
@@ -0,0 +1,28 @@
+```javascript
+// pages/about.js
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+
+export default function About() {
+  const router = useRouter();
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+    return () => setIsMounted(false);
+  }, []);
+
+  const handleClick = () => {
+    if (isMounted) {
+      router.push('/');
+    }
+  };
+
+  return (
+    <div>
+      <h1>About Page</h1>
+      <button onClick={handleClick}>Go back to Home</button>
+    </div>
+  );
+}
+```
